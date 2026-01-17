93d8c59b1f048b101c29065f86ed94cb1c8c6bf3
keyonnejames <115831465+KeyonneJames@users.noreply.github.com>
2026-01-17 23:32:07 +0000

    finished solving the containsDuplicate problem. and working on new problem validAnangram.

--- containsDuplicate.java
+++ containsDuplicate.java
@@ -1,4 +1,14 @@
-public static void containsDuplicate(){
+// solution for containsDuplicate in o(1) 
 
-    
-}
+    public boolean hasDuplicate(int[] nums) {
+        HashMap <Integer, Integer> storeNumbers = new HashMap<>(); //create hash map
+        
+        for(int i = 0; i < nums.length; i++){
+            if(storeNumbers.containsKey(nums[i])){ // if hashmap has the number(duplicate), return true
+                return true;
+            }else{
+                storeNumbers.put(nums[i], i); // else add it to the hashmap
+            }
+        }
+        return false;
+    }
